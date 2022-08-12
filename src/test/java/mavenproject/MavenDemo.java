6663de360f35c66640d49a2e1cdf429fc88dba5d
SMAHADEV916 <110241727+SMAHADEV916@users.noreply.github.com>
2022-08-12 15:22:31 +0530

    Add files via upload
    commit for the execution

---
+++ src/test/java/mavenproject/MavenDemo.java
@@ -0,0 +1,11 @@
+package mavenproject;
+
+public class MavenDemo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("maven project");
+		
+	}
+
+}
---
+++ src/test/java/mavenproject/MavenProjectTwo.java
@@ -0,0 +1,11 @@
+package mavenproject;
+
+public class MavenProjectTwo {
+
+	public static void main(String[] args) {
+	
+		
+		System.out.println("GitHub");
+	}
+
+}
